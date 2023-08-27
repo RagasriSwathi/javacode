1722d22e7f6f7126f86be81de3c030ae9111a9d9
RagasriSwathi <143309912+RagasriSwathi@users.noreply.github.com>
2023-08-27 01:22:39 -0700

    Update hello.java again

--- hello.java
+++ hello.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 7551830569eeda352032605463418ae0e4f7b50f
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
