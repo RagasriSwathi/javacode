bd664bfeb2a06c94aee682062d1377e86732f16e
Ragasri swathi <ragasri178@gmail.com>
2023-08-27 01:21:03 -0700

    updated code

--- hello.java
+++ hello.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-#Java program 1
-=======
-#java Program 1
->>>>>>> 2b6432b0dc524bc961be0b51c2f4050ede992708
+
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
